b1f72714d749383189dc92207ff7466de2658e56
Peter Thompson <peter.thompson@dunelm.org.uk>
2020-12-06 23:10:39 +0100

    Store input path in config

--- lib/advent/input.ex
+++ lib/advent/input.ex
@@ -11,7 +11,19 @@ defmodule Advent.Input do
   For example, the input for day one should be in a file called "day01".
   """
   def read!(suffix) do
-    File.open!("./input/day" <> suffix, [:read, :utf8])
-    |> IO.read(:all)
+    input_file = Path.join([input_root() <> "/day" <> suffix])
+
+    if File.exists?(input_file) do
+      File.open!(input_file, [:read, :utf8])
+      |> IO.read(:all)
+    else
+      raise "Input file '#{input_file}' does not exist"
+    end
+  end
+
+  defp input_root() do
+    # TODO: add default input_path/error if unset
+    Application.get_env(:aoc2020, __MODULE__)
+    |> Keyword.get(:input_path)
   end
 end
